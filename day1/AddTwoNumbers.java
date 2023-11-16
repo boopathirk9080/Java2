6f4c07e42d2112d9cde938d18f344e0987e9a461
Boopathi_rk <138640788+boopathirk9080@users.noreply.github.com>
2023-11-16 21:43:39 +0530

    Add files via upload

---
+++ day1/AddTwoNumbers.java
@@ -0,0 +1,9 @@
+
+public class SubTwoNumbers {
+    public static void main(String[] args) {
+        int num1=32;
+        int num2=17;
+        int sub=num1-num2;
+        System.out.println(sub);
+    }
+}
