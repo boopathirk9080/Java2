768e9fcc75374152bfa36356e0cb57d775aaf5de
Boopathi_rk <138640788+boopathirk9080@users.noreply.github.com>
2023-11-20 21:30:00 +0530

    Update CountSumOfDigits.java

--- day 2/CountSumOfDigits.java
+++ day 2/CountSumOfDigits.java
@@ -3,14 +3,7 @@ public class LargestOf3Num{
  int a=15;
  int b=25;
  int c=35;
- if(a>b&&a>c){
-    System.out.println("a is largest num :"+a);
-    }
-    else if(b>a&&b>c){
-        System.out.println("b is largest num:"+b);
-    }
-    else
-    System.out.println("c is largest num :"+c);
+    System.out.println(a+b+c);
     }
     }
 
