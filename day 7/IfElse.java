8d452b34f6316d418a2fb6b09a2ea6d5536585ac
Boopathi_rk <138640788+boopathirk9080@users.noreply.github.com>
2023-11-22 22:21:07 +0530

    Create IfElse.java

---
+++ day 7/IfElse.java
@@ -0,0 +1,10 @@
+public class SimpleIfElseExample {
+    public static void main(String[] args) {
+        int num = 15;
+        if (num > 10) {
+            System.out.println("The number is greater than 10.");
+        } else {
+            System.out.println("The number is not greater than 10.");
+        }
+    }
+}
